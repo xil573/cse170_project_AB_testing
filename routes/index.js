8eff672a25a20d510f7030d6ce239f6626183412
Xindi LI <xil573@eng.ucsd.edu>
2017-03-07 16:12:32 -0800

    GA

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,11 @@ var friends = require('../people.json');
  */
 
 exports.view = function (req, res) {
+	friends["verb"] = false;
     res.render('index', friends);
+};
+
+exports.verb = function (req, res) {
+	friends["verb"] = true;
+	res.render('index', friends);
 };
